bff501d0c7445176782ac17cb50172ad7d49fdff
lvhuiyang <964790821@qq.com>
2016-03-21 21:30:06 +0800

    wechat

--- wechat.py
+++ wechat.py
@@ -1,39 +1,33 @@
 # -*- coding: utf-8 -*-
 # from config import app
 from flask import Flask, request
-
-from wechat_sdk import WechatBasic
-from wechat_sdk import WechatConf
 import hashlib
 
-conf = WechatConf(
-    token='lvhuiyang',
-    appid='wx35028b78608d38f9',
-    appsecret='107da9b468d3cf7427dc439de6405d01',
-    # encrypt_mode='safe',  # 可选项：normal/compatible/safe，分别对应于 明文/兼容/安全 模式
-    # encoding_aes_key='your_encoding_aes_key'  # 如果传入此值则必须保证同时传入 token, appid
-)
-
 app = Flask(__name__)
-wechat = WechatBasic(conf=conf)
 
-# 微信开发
+
 @app.route('/wechat')
 def wechat():
-    timestamp = request.args('timestamp')
-    nonce = request.args('nonce')
-    echostr = request.args('echostr')
-    token = 'lvhuiyang'
-    li = [token, timestamp, nonce]
-    li.sort()
-    sha1 = hashlib.sha1()
-    map(sha1.update, li)
-    hashcode = sha1.hexdigest()
+    if request.method == 'GET':
+        timestamp = request.args.get('timestamp')
+        nonce = request.args.get('nonce')
+        echostr = request.args.get('echostr')
+        token = 'lvhuiyang'
+        li = [token, timestamp, nonce]
+        li.sort()
+        sha1 = hashlib.sha1()
+        map(sha1.update, li)
+        hashcode = sha1.hexdigest()
 
-    if hashcode == request.args('signature'):
-        return echostr
+        if hashcode == request.args.get('signature'):
+            return echostr
+        else:
+            print "There is an error"
+    elif request.method == 'POST':
+        post_body = request.data
+        print post_body
     else:
-        print "There is an error"
+        pass
 
 
 if __name__ == "__main__":
