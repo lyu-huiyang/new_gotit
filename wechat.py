ba8eae0f2fdf94bf60ad4ef411197e079b7a1c4d
lvhuiyang <964790821@qq.com>
2016-03-22 09:45:47 +0000

    use wechat-sdk

--- wechat.py
+++ wechat.py
@@ -1,9 +1,20 @@
 # -*- coding: utf-8 -*-
-# from config import app
-from flask import Flask, request
+from flask import Flask, request, make_response
+import xml.etree.ElementTree as ET
 import hashlib
 
 app = Flask(__name__)
+from wechat_sdk import WechatConf
+
+conf = WechatConf(
+    token='lvhuiyang',
+    appid='wx35028b78608d38f9',
+    appsecret='107da9b468d3cf7427dc439de6405d01',
+)
+
+from wechat_sdk import WechatBasic
+
+wechat = WechatBasic(conf=conf)
 
 
 @app.route('/wechat')
@@ -18,14 +29,24 @@ def wechat():
         sha1 = hashlib.sha1()
         map(sha1.update, li)
         hashcode = sha1.hexdigest()
-
+        '''
         if hashcode == request.args.get('signature'):
             return echostr
         else:
-            print "There is an error"
+            print "There is an error"'''
+        if wechat.check_signature(hashcode, timestamp, nonce):
+            print 'Accept'
+        else:
+            print 'Wrong'
     elif request.method == 'POST':
-        post_body = request.data
-        print post_body
+        print 'HelloWorld'
+        rec = request.stream.read()
+        xml_rec = ET.fromstring(rec)
+        print '###############', xml_rec
+        fromu = xml_rec.find('FromUserName').text
+        event = xml_rec.find('Event')
+        event_key = xml_rec.find('EventKey')
+        content = xml_rec.find('Content')
     else:
         pass
 
