77f980060588b7157437acc246e567a859cab92c
lvhuiyang <964790821@qq.com>
2016-03-23 19:23:53 +0800

    wechat-test

--- spider/test.py
+++ spider/test.py
@@ -1,14 +1,29 @@
 # -*- coding: utf-8 -*-
-import time
-import random
+import xml.etree.ElementTree as ET
 
+test_message = """
+        <xml>
+        <ToUserName><![CDATA[gh_113ae65cd7ed]]></ToUserName>
+        <FromUserName><![CDATA[odabgv1gl5U-ZcEmRIJB3vFrw41M]]></FromUserName>
+        <CreateTime>1458699029</CreateTime>
+        <MsgType><![CDATA[text]]></MsgType>
+        <Content><![CDATA[。]]></Content>
+        <MsgId>6265064624663882806</MsgId>
+        </xml>
+        """
+root = ET.fromstring(test_message)
+to_user_name = root.findall('ToUserName')[0].text
+from_user_name = root.findall('FromUserName')[0].text
+create_time = root.findall('CreateTime')[0].text
+message_type = root.findall('MsgType')[0].text
+content = root.findall('Content')[0].text
+message_id = root.findall('MsgId')[0].text
+return_message = """<xml>
+<ToUserName><![CDATA[%s]]></ToUserName>
+<FromUserName><![CDATA[%s]]></FromUserName>
+<CreateTime>%s</CreateTime>
+<MsgType><![CDATA[%s]]></MsgType>
+<Content><![CDATA[%s]]></Content>
+</xml>""" % (to_user_name, from_user_name,create_time,message_type,content)
 
-def get_random_str():
-    time_test = time.time()
-    time_int = int(time_test * 10)
-    time_str = str(time_int)
-    random_number = random.randrange(101, 999)
-    random_str = str(random_number)
-    final_random_str = time_str + random_str
-
-    return final_random_str
+print return_message
--- wechat.py
+++ wechat.py
@@ -1,23 +1,12 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, request, make_response
-import xml.etree.ElementTree as ET
+from flask import request
 import hashlib
+import xml.etree.ElementTree as ET
 
-app = Flask(__name__)
-from wechat_sdk import WechatConf
-
-conf = WechatConf(
-    token='lvhuiyang',
-    appid='wx35028b78608d38f9',
-    appsecret='107da9b468d3cf7427dc439de6405d01',
-)
-
-from wechat_sdk import WechatBasic
-
-wechat = WechatBasic(conf=conf)
+from config import app
 
 
-@app.route('/wechat')
+@app.route('/wechat', methods=['GET', 'POST'])
 def wechat():
     if request.method == 'GET':
         timestamp = request.args.get('timestamp')
@@ -29,29 +18,29 @@ def wechat():
         sha1 = hashlib.sha1()
         map(sha1.update, li)
         hashcode = sha1.hexdigest()
-        '''
         if hashcode == request.args.get('signature'):
             return echostr
         else:
-            print "There is an error"'''
-        if wechat.check_signature(hashcode, timestamp, nonce):
-            print 'Accept'
-            return echostr
-        else:
-            print 'Wrong'
-    elif request.method == 'POST':
-        print 'HelloWorld'
-        rec = request.stream.read()
-        xml_rec = ET.fromstring(rec)
-        print '###############', xml_rec
-        fromu = xml_rec.find('FromUserName').text
-        event = xml_rec.find('Event')
-        event_key = xml_rec.find('EventKey')
-        content = xml_rec.find('Content')
+            print "There is an error"
     else:
-        pass
+        print 'HelloWorld'
+        message = request.data  # get the data from post request
+        root = ET.fromstring(message)
+        to_user_name = root.findall('ToUserName')[0].text
+        from_user_name = root.findall('FromUserName')[0].text
+        create_time = root.findall('CreateTime')[0].text
+        message_type = root.findall('MsgType')[0].text
+        content = root.findall('Content')[0].text
+        if content == "综合成绩查询":
+            pass
+        message_id = root.findall('MsgId')[0].text
+        return_message = """<xml>
+        <ToUserName><![CDATA[%s]]></ToUserName>
+        <FromUserName><![CDATA[%s]]></FromUserName>
+        <CreateTime>%s</CreateTime>
+        <MsgType><![CDATA[%s]]></MsgType>
+        <Content><![CDATA[%s]]></Content>
+        </xml>""" % (from_user_name, to_user_name, create_time, message_type, content)
+        return return_message
 
 
-if __name__ == "__main__":
-    app.debug = True
-    app.run(host='0.0.0.0', port=80)
